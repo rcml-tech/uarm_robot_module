4c0a192063738501f9cd956d0323df712f06c822
unknown <tarpeda-kirill@yandex.ru>
2015-07-10 13:36:52 +0500

    no message

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -179,57 +179,6 @@ void loop()
           servoR.write(mass[4],mass[5]);
           servoHandRot.write(mass[6],mass[7]);
         break;
-=======
-  uarm.init();          // initialize the uArm position
-  uarm.setServoSpeed(SERVO_R, 50);  // 0=full speed, 1-255 slower to faster
-  uarm.setServoSpeed(SERVO_L, 50);  // 0=full speed, 1-255 slower to faster
-  uarm.setServoSpeed(SERVO_ROT, 50); // 0=full speed, 1-255 slower to faster
-}
-
-void loop(void) {
-  uarm.calibration();   // if corrected, you could remove it, no harm though
-  if (Serial.available()) {
-    rxBuf = Serial.read();
-    Serial.print("rxBuf = " + String(rxBuf) + "\n");
-    
-    if (stateMachine == 0) {
-      stateMachine = rxBuf == 0xFF ? 1 : 0;
-    } else if (stateMachine == 1) {
-      if (rxBuf == 0xEE) {
-        stateMachine = 2;
-      } else if (rxBuf == 0xDD) {
-        stateMachine = 3;
-      } else {
-        stateMachine = 0;
-      }
-    } else if (stateMachine == 2) {
-      dataBuf[counter++] = rxBuf;
-      if(counter > 7) {
-        stateMachine = 0;
-        counter=0;
-        
-        *((char *)(&stretchTemp )  )  = dataBuf[1]; 
-        *((char *)(&stretchTemp )+1)  = dataBuf[0]; 
-        *((char *)(&heightTemp  )  )  = dataBuf[3]; 
-        *((char *)(&heightTemp  )+1)  = dataBuf[2]; 
-        *((char *)(&rotationTemp)  )  = dataBuf[5];
-        *((char *)(&rotationTemp)+1)  = dataBuf[4]; 
-        *((char *)(&handRotTemp )  )  = dataBuf[7]; 
-        *((char *)(&handRotTemp )+1)  = dataBuf[6]; 
-        
-        Serial.print("stretch = " + String(stretchTemp) + "; height = " + String(heightTemp) + "; rotation = " + String(rotationTemp) + "; handRot = " + String(handRotTemp) + ";\n");
-        
-        uarm.setPosition(stretchTemp, heightTemp, rotationTemp, handRotTemp);
-      }
-    } else if (stateMachine == 3) {
-      stateMachine = 0;
-      if (rxBuf) {
-        uarm.gripperCatch();
-        Serial.print("gripperCatch\n");
-      } else {
-        uarm.gripperRelease();
-        Serial.print("gripperRelease\n");
->>>>>>> bea2b19c183c0fccc0dbb2e7aeadd60bfa027926
       }
       msg = "";
     }
